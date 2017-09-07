2b6153b127aafca76e9a8df0dfd9c3efe8663d3b
alechp <me@alechp.com>
2017-09-07 12:19:25 -0700

    Added config and client instantiation to pusher

--- pusher.js
+++ pusher.js
@@ -1,4 +1,11 @@
+const request = require('graphql-request')
+const config = require('./config.js');
 
+const client = new GraphQLClient(config.GCOOL_API_SIMPLE_ENDPOINT, {
+  headers: {
+    Authorization: `Bearer ${config.GCOOL_API_AUTH_TOKEN}`
+  }
+});
 class QueryPusher {
   constructor(queries, options) {
     this.qs = queries;
